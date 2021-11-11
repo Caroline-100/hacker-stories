b47f89a8784ca01329c11d1cc0ba3371838ecd3b
Caroline-100 <47947548+Caroline-100@users.noreply.github.com>
2021-11-11 21:55:26 +0100

    move list into App
    App pass infos to  List

--- src/App.js
+++ src/App.js
@@ -10,80 +10,104 @@ const welcome = {
   title: title,
 };
 //Render an array
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Eloquent JavaScript",
-    url: "https://eloquentjavascript.net/",
-    author: "Marijn Haverbeke",
-    num_comments: 3,
-    points: 3,
-    objectID: 2,
-  },
-  {
-    title: "Twitter Bootsrap 4",
-    url: "https://www.syncfusion.com/ebooks/twitterbootstrap4-succinctly/the-grids-the-grids-the-beautiful-grids",
-    author: "Peter Shaw",
-    num_comments: 2,
-    points: 4,
-    objectID: 3,
-  },
-];
-// const arrayNumbers = [1,2,34,5,6];
-const arrayNumbs = list.map(function (item) {
-  return <li key={item.objectID}>{item.author}</li>;
-});
+// const list = [
+//   {
+//     title: "Reacts",
+//     url: "https://reactjs.org/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+//   {
+//     title: "Eloquent JavaScript",
+//     url: "https://eloquentjavascript.net/",
+//     author: "Marijn Haverbeke",
+//     num_comments: 3,
+//     points: 3,
+//     objectID: 2,
+//   },
+//   {
+//     title: "Twitter Bootsrap 4",
+//     url: "https://www.syncfusion.com/ebooks/twitterbootstrap4-succinctly/the-grids-the-grids-the-beautiful-grids",
+//     author: "Peter Shaw",
+//     num_comments: 2,
+//     points: 4,
+//     objectID: 3,
+//   },
+// ];
+// // const arrayNumbers = [1,2,34,5,6];
+// const arrayNumbs = list.map(function (item) {
+//   return <li key={item.objectID}>{item.author}</li>;
+// });
 
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "Eloquent JavaScript",
+      url: "https://eloquentjavascript.net/",
+      author: "Marijn Haverbeke",
+      num_comments: 3,
+      points: 3,
+      objectID: 2,
+    },
+    {
+      title: "Twitter Bootsrap 4",
+      url: "https://www.syncfusion.com/ebooks/twitterbootstrap4-succinctly/the-grids-the-grids-the-beautiful-grids",
+      author: "Peter Shaw",
+      num_comments: 2,
+      points: 4,
+      objectID: 3,
+    },
+  ];
   return (
     <div>
       <h1>Hello World {title}</h1>
-      <Search/>
+      {/* <Search/> */}
       <h1>
         {welcome.greeting} {welcome.title} {greeting("Caroline")}
       </h1>
       <hr />
-      <List />
+      <List list={stories}/>
     </div>
   );
 }
-const Search = () => {
-  function handleChangeInput (event) {
-    console.log(event.target.value)
-  }
+
+const List = (props) => {
   return (
     <div>
-      <label htmlFor="search">Search :</label>
-      <input onChange={handleChangeInput} id="search" type="text" />
-    </div>
-  );
-}
-const List = () => {
-  return (
-    <div>
-      <ul>{arrayNumbs}</ul>
+      {/* <ul>{arrayNumbs}</ul> */}
       <ul>
-        {list.map(function (item) {
+        {props.list.map(function (item) {
           return <li key={item.objectID}>{item.num_comments}</li>;
         })}
       </ul>
       <hr />
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
           <div key={item.objectID}>
             <LinkTitle url={item.url} title={item.title} />
@@ -102,7 +126,7 @@ function LinkTitle(props) {
       <a href={props.url}>{props.title}</a>
     </span>)
 }
-// child of List return num of comments and Points
+// // child of List return num of comments and Points
 function NumofCommentAndPoints(props) {
   return (
     <ul>
